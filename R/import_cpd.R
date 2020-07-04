b4b053b1fd4743480817e2e34941770e4522db2b
Rhian Davies <thetrianglegirl@gmail.com>
2020-07-04 12:50:50 +0100

    Fix note

--- R/import_cpd.R
+++ R/import_cpd.R
@@ -3,6 +3,7 @@
 #' @importFrom purrr map
 #' @importFrom tibble tibble
 #' @importFrom tidyr unnest
+#' @importFrom rlang .data
 #' @param path Path to CPD records
 #' @return a [tibble][tibble::tibble-package]
 #' @export
@@ -12,9 +13,9 @@ import_cpd = function(path =  "."){
 
   df =
     df %>%
-    mutate(data = map(.x = filename, ~yaml_to_tibble(file = glue("{path}/{.x}")))) %>%
+    mutate(data = purrr::map(.x = filename, ~yaml_to_tibble(file = glue("{path}/{.x}")))) %>%
     select(-filename) %>%
-    unnest(cols = c(data))
+    unnest(cols = c(.data$data))
   return(df)
 }
 
