0a58c5ab60ec5cd7c32fc680fe69578e8570efad
Rhian Davies <thetrianglegirl@gmail.com>
2020-07-04 12:28:24 +0100

    Add import_cpd function

---
+++ R/import_cpd.R
@@ -0,0 +1,20 @@
+#' import_cpd
+#' @importFrom dplyr mutate select
+#' @impotFrom purrr map
+#' @importFrom tibble tibble
+#' @importFrom tidyr unnest
+#' @param path Path to CPD records
+#' @return a [tibble][tibble::tibble-package]
+import_cpd = function(path =  "."){
+  filename = list.files(path = path, pattern = "*.md", recursive = TRUE)
+  df = tibble(filename = filename)
+
+  df =
+    df %>%
+    mutate(data = map(.x = filename, ~yaml_to_tibble(file = glue("{path}/{.x}")))) %>%
+    select(-filename) %>%
+    unnest(cols = c(data))
+  return(df)
+}
+
+
