7de5bce7a5a40373c2e8aa15f84bc533df946eed
Rhian Davies <thetrianglegirl@gmail.com>
2020-07-04 12:16:50 +0100

    Pass checks

--- R/yaml_to_tibble.R
+++ R/yaml_to_tibble.R
@@ -2,6 +2,7 @@
 #' @importFrom yaml read_yaml
 #' @importFrom tibble as_tibble
 #' @param file File to be converted
+#' @return a [tibble][tibble::tibble-package]
 yaml_to_tibble = function(file) {
   read_yaml(file) %>%
     as_tibble()
